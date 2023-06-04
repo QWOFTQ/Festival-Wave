b63c013c4a287cf913063f7fe7700b3e7bbb80d2
QWOFTQ <rkdcofls3890@naver.com>
2023-06-04 22:21:49 +0900

    changeImage

---
+++ js/main.js
@@ -0,0 +1,97 @@
+function changeImage(x, image) {
+  if (x == 1) {
+    image.src = 'img/석조전.gif'
+  }
+
+  if (x == 2) {
+    image.src = 'img/iced-americano.png'
+  }
+
+  if (x == 3) {
+    image.src = 'img/달빛기행.gif'
+  }
+
+  if (x == 4) {
+    image.src = 'img/iced-cappuccino.png'
+  }
+
+  if (x == 5) {
+    image.src = 'img/경복궁.gif'
+  }
+
+  if (x == 6) {
+    image.src = 'img/iced-espresso.png'
+  }
+
+  if (x == 7) {
+    image.src = 'img/DDP.jpg'
+  }
+
+  if (x == 8) {
+    image.src = 'img/iced-latte.png'
+  }
+
+  if (x == 9) {
+    image.src = 'img/피나클랜드 튤립 수선화.jpg'
+  }
+
+  if (x == 10) {
+    image.src = 'img/hot-americano.png'
+  }
+
+  if (x == 11) {
+    image.src = 'img/hot-cappuccino.png'
+  }
+
+  if (x == 12) {
+    image.src = 'img/피나클랜드 불꽃.jpg'
+  }
+
+  if (x == 13) {
+    image.src = 'img/태안.gif'
+  }
+
+  if (x == 14) {
+    image.src = 'img/hot-espresso.png'
+  }
+
+  if (x == 15) {
+    image.src = 'img/hot-latte.png'
+  }
+
+  if (x == 16) {
+    image.src = 'img/부소산.jpg'
+  }
+
+  if (x == 17) {
+    image.src = 'img/휴애리.gif'
+  }
+
+  if (x == 18) {
+    image.src = 'img/smoothie-1.png'
+  }
+
+  if (x == 19) {
+    image.src = 'img/마노르블랑.jpg'
+  }
+
+  if (x == 20) {
+    image.src = 'img/smoothie-2.png'
+  }
+
+  if (x == 21) {
+    image.src = 'img/smoothie-3.png'
+  }
+
+  if (x == 22) {
+    image.src = 'img/고사리.jpg'
+  }
+
+  if (x == 23) {
+    image.src = 'img/반려동물.jpg'
+  }
+
+  if (x == 24) {
+    image.src = 'img/smoothie-4.png'
+  }
+}
