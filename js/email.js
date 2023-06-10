4ec06c7d74c257acc27bd8610b0cecde056af7f9
QWOFTQ <rkdcofls3890@naver.com>
2023-06-10 22:25:17 +0900

    alert

--- js/email.js
+++ js/email.js
@@ -8,6 +8,6 @@ function sendMail() {
   emailjs
     .send('service_462bnng', 'template_z1010u2', params)
     .then(function (res) {
-      alert('Success!' + res.status)
+      alert(res.status)
     })
 }
